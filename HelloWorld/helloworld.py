87fd485aef6bc3d7379e134dfb56bc1116fb0a8b
Ronny <set@aliyun.com>
2016-11-01 15:26:21 +0800

    helloworld --python输入输出，以及设置file模板

---
+++ HelloWorld/helloworld.py
@@ -0,0 +1,7 @@
+#coding=utf-8
+__author__ = 'ronny'
+__mtime__ = '2016/11/1'
+
+print("hello world")
+name = input('please input your name:')
+print('hello',name)
---
+++ HelloWorld/test_template.py
@@ -0,0 +1,24 @@
+#coding=utf-8
+"""
+ @desc:
+ @author: ronny
+ @contact: set@aliyun.com
+ @site: www.lemon.pub
+ @software: PyCharm  @since:python 3.5.2 on 2016/11/1.14:42
+ """
+"""
+python中多行注释使用三个单引号或三个双引号
+这个例子是测试python file template，使创建.py时默认自带如上的注释
+设置方法：Settings-->Editor-->Code Style-->File and Code Templates-->python script
+#coding=utf-8
+#set( $SITE = "www.lemon.pub")
+ '''
+ @desc:
+ @author: ${USER}
+ @contact: set@aliyun.com
+ @site: ${SITE}
+ @software: ${PRODUCT_NAME}  @since:python 3.5.2 on ${DATE}.${TIME}
+ '''
+"""
+
+print("欢迎ronny --www.lemon.pub")
