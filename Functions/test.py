a021f0de3dd1a33ac4deef99ef98d4484320d6ac
Ronny <set@aliyun.com>
2016-11-08 14:39:38 +0800

    +update Functions 更新对调用其他py文件的函数的说明

--- Functions/test.py
+++ Functions/test.py
@@ -8,9 +8,9 @@
  @software: PyCharm  @since:python 3.5.2(32bit) on 2016/11/7.13:43
 """
 
-from definefunctions import my_abs
+from definefunctions import my_abs     #调用definefunctions.py文件中的my_abs方法  .py后缀在调用时省略掉
 print(my_abs(-100))
 
-from definefunctions import my_max
+from definefunctions import my_max     #调用definefunctions.py文件中的my_max方法
 print(my_max(3,8))
 
