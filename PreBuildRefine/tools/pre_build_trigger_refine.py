54b677f34e5fae0068751a2770a43d904d402164
Ronny <set@aliyun.com>
2016-11-11 19:52:34 +0800

    +add update prebuild --prebuild_trigger_refine.py

--- PreBuildRefine/tools/pre_build_trigger_refine.py
+++ PreBuildRefine/tools/pre_build_trigger_refine.py
@@ -9,6 +9,7 @@
 """
 import os
 import logging
+import commands
 '''
 几个重要的概念
 
@@ -17,9 +18,8 @@ import logging
     Filter 过滤器，提供了更好的粒度控制，它可以决定输出哪些日志记录。
     Formatter 格式化器，指明了最终输出中日志记录的布局。
 '''
-def GetLog():
-    """Initialize the global log"""
 
+def GetLog():
     logger = logging.getLogger(__name__)   #创建记录器
     console = logging.StreamHandler()    #创建处理器
     formatter = logging.Formatter('%(asctime)s %(levelname)s(%(thread)d)    \
@@ -28,42 +28,65 @@ def GetLog():
     # %(levelname)s 打印日志级别名称
     # %(thread)d 	打印线程id
     # %(message)s 	打印日志信息
-
     logger.addHandler(console)     #为记录器增加处理器
     console.setFormatter(formatter)   #设置一个格式化器formatter
     file = logging.FileHandler(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'pre_build_trigger.log'))
-    # logger.warning('warn message')
-    # logger.setLevel(logging.INFO)     #设置日志级别
-    '''当单独运行pre_build_trigger_refine.py时print(__name__)显示为__main__,
-       当其他py调用这个py中的GetLog方法时:
-       from pre_build_trigger_refine import GetLog
+    file.setFormatter(formatter)
+    logger.addHandler(file)
+    logger.setLevel(logging.INFO)
 
-       GetLog()
-       __name__显示为pre_build_trigger_refine
-
-    '''
-    print(logger)
-    print(__name__)
-    # logger = logging.getLogger(__name__)
-    # console = logging.StreamHandler()
-    # formatter = logging.Formatter('%(asctime)s %(levelname)s(%(thread)d)    \
-    #         : %(message)s')
-    # console.setFormatter(formatter)
-    # logger.addHandler(console)
+    logger.setLevel(logging.INFO)     #设置日志级别
+    #当单独运行pre_build_trigger_refine.py时print(__name__)显示为__main__,
+    # 当其他py调用这个py中的GetLog方法时:
+    # from pre_build_trigger_refine import GetLog
     #
-    # file = logging.FileHandler(
-    #     os.path.join(os.path.abspath(os.path.dirname(__file__)), 'pre_build_trigger.log'))
-    # file.setFormatter(formatter)
-    # logger.addHandler(file)
-    # logger.setLevel(logging.INFO)
-    # return logger
+    # GetLog()
+    # __name__显示为pre_build_trigger_refine
+    return  logger
+
+LOG = GetLog()    ###LOG为我创建的logger对象，以后用这个对象去打印log，在每个函数头尾打印log，以方便定位错误发生在哪个函数
+
+def run_shell_command(cmd):
+    sys = os.name
+    if sys == 'posix':
+        LOG.info(cmd)
+        return commands.getstatusoutput(cmd)
+    else:
+        return 'System of the PC is not a posix'
+
+def get_env(ev):
+    return os.environ.get(ev)
+
+def get_change_content(gerrit_ip="10.27.254.101", gerrit_port="29000"):
+    # change_id = get_env("JAVA_HOME")     ##参数为JAVA_HOME时echo：C:\Program Files\Java\jdk1.7.0_80
+    # print(gerrit_ip,gerrit_port,change_id)
+    change_id = get_env("GERRIT_CHANGE_ID")
+    command = "ssh -p %s %s gerrit query %s --file --current-patch-set" % (gerrit_port,gerrit_ip,change_id)
+    status,content = run_shell_command(command)
+    if status == 0:
+        LOG.info("get_change_content = %s" % content)
+        return content
+    else:
+        LOG.error("get change content error")
+        return None
+
+
+def update_trigger_config():
+    LOG.info('update_trigger_config in')
+    change_content = get_change_content()
+    LOG.info('update_trigger_config out')
 
 
 def main():
-    GetLog()
+    LOG.info('main in')
 
+    update_trigger_config()
+    # run_shell_command('aa')
+    LOG.info('main out')
 
 
 
 if __name__ == '__main__':
     main()
+
+
